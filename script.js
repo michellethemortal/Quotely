0da5a2e883ec7579a450b2bf3e8c4f5340600185
Michelle Mcathi <mcathimichelle@gmail.com>
2025-11-20 03:29:48 +0200

    More quotes

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 const quotes = [
   { text: '"Love thy neighbour."', author: 'Jesus Christ', source: 'Son of God' },
-  { text: '"The only thing we have to fear is fear itself."', author: 'Franklin D. Roosevelt'},
+  { text: '"You rock! Thank you for checking out my work"', author: 'Michelle'},
   { text: '"I think, therefore I am."', author: 'René Descartes', source: 'Discourse on the Method' },
-  { text: '"Be yourself, everyone else is already taken."', author: 'Oscar Wilde', source: 'Quote' }
+  { text: '"Be yourself, everyone else is already taken."', author: 'Oscar Wilde', source: 'Quote' },
+  { text: '"Find someone who loves who you are, not who you could be."', author: 'Anonymous', source: 'Quote' },
+  { text: '"When we are no longer able to change a situation - we are challenged to change ourselves."', author: 'Viktor Frankl', source: "Man's search for meaning" },
+  { text: '"Find someone who loves who you are, not who you could be."', author: 'Anonymous', source: 'Quote' },
+  { text: '"The best way to keep a prisoner from escaping is to make sure he never knows he is in prison."', author: 'Fyodor Dostoevsky', source: 'Quote' },
+  { text: "The only one that's judging you is yourself.", author: 'Tame Impala', source: 'Alter Ego - The Innerspeaker Album' },
+
 ];
 
 const quoteText = document.getElementById('quote-text');
